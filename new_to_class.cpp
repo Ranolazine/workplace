ebaab11c7d6b335411d7b703b032ff0f2b99b5a3
unknown <zd190999246@gmail.com>
2015-07-08 09:28:08 +0800

    about virtual function

---
+++ new_to_class.cpp
@@ -0,0 +1,37 @@
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <ctime>
+#include <iostream>
+#include <string>
+#include <limits>
+//how to use class 	7.07
+
+using namespace std;
+
+class A
+{
+public:
+	virtual void Print(){cout<<'A'<<endl;}
+};
+
+class B: public A
+{
+public:
+	virtual void Print(){cout<<'B'<<endl;}
+};
+
+void PrintInfo(A & r)
+{
+	r.Print();
+}
+int main()
+{
+	A a;B b;
+	PrintInfo(a);
+	PrintInfo(b);
+
+	std::cout << "\nPress ENTER to continue...";
+	std::cin.ignore( std::numeric_limits<std::streamsize>::max(), '\n' );
+	return 0;
+}
